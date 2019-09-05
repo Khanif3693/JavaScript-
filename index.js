d80a25c00d6206ec1a231b587d68e7c91b200153
Khanif3693 <khanha@oregonstate.edu>
2019-09-05 18:48:59 -0400

    Sample project with Jquery, Bootstrap, HTML and CSS

--- index.js
+++ index.js
@@ -1,133 +1,44 @@
-//Question:1
+// Wait for the DOM to be ready
+var data = {};
+var user = { Muzz: 123 };
+var copy = $("#Info").html();
 
-var employees = [
-  { id: 10, name: "John", experience: 5 },
-  { id: 15, name: "Mathew", experience: 7 },
-  { id: 20, name: "keith", experience: 12 },
-  { id: 25, name: "louis", experience: 5 },
-  { id: 30, name: "mark", experience: 4 },
-  { id: 35, name: "Ash", experience: 3 },
-  { id: 40, name: "robert", experience: 6 }
-];
+$(document).ready(function() {
+  $.getJSON("bikerentals.json", function(json) {
+    console.log("Success reading Json File");
+    data = json;
+  });
+  $("#datable_1 tbody").on("click", "tr", function() {
+    $(this).toggleClass("selected");
+  });
+  $("#button").click(function() {
+    alert(table.rows(".selected").data().length + " row(s) selected");
+  });
 
-let IDs = employees.map(item => {
-  return item.id;
-}); // map function to return only ids
-let total = employees.map(item => {
-  return item.experience;
-}); // map function to return experience
+  $("#SignIn_Submit").click(function(event) {
+    // console.log(data);
 
-function eg7(item) {
-  return item.experience > 5;
-}
+    if (user[$("#SignIn_Username").val()] == $("#SignIn_Password").val()) {
+      console.log("SignIn Success");
+      $("#hide").hide();
 
-console.log(IDs);
-console.log(total.reduce((a, b) => a + b, 0));
-console.log(employees.filter(eg7));
+      $("#change").html(copy);
 
-// Question 2
-
-let cat = {
-  catName: "Kitty",
-  sayName() {
-    return this.catName;
-  }
-};
-
-let sayName = cat.sayName();
-console.log(sayName);
-
-//Fix to Print out ‘Kitty’
-
-//Question 3
-
-class user {
-  constructor(name, location) {
-    (this.name = name), (this.location = location);
-  }
-  displayName(name, location) {
-    return this.name + " is in " + this.location;
-  }
-}
-
-var user1 = new user("Marlabs", "New Jersey");
-
-console.log(user1.displayName());
-
-//Print: ‘Marlabs is in New Jersey’ (DO NOT create variables outside the User class)
-
-//Question 4
-
-function myName(fName, lName) {
-  // console.log(`my name is ${fName} ${lName}`);
-
-  function fullDetails(companyName) {
-    const value = `my name is ${fName} ${lName} and I work for ${companyName}`;
-    return value;
-  }
-
-  return fullDetails(this.companyName);
-}
-
-console.log(myName.call({ companyName: "Marlabs" }, "Althaf", "Pattan"));
-
-//Print: ‘My name is Althaf Pattan and I work for Marlabs’
-
-//Question 5
-
-var pokemon = {
-  firstname: "Pika",
-  lastname: "Chu ",
-  getPokeName: function() {
-    var fullname = this.firstname + " " + this.lastname;
-    return fullname;
-  }
-};
-
-var pokemonName = function(snack, hobby) {
-  console.log(this.getPokeName() + "I choose you!");
-  console.log(this.getPokeName() + " loves " + snack + " and " + hobby);
-};
-
-console.log(pokemonName.apply(pokemon, ["sushi", "algorithm"]));
-
-//Print: ‘Pika Chu  loves sushi and algorithms’
-
-//Question 6
-
-var x = 5;
-var promise1 = new Promise(function(resolve, reject) {
-  if (x == 5) {
-    let data = {
-      name: "Marlabs",
-      address: {
-        state: "NJ",
-        zip: 084444
-      }
-    };
-    console.log(data.name, "adress:");
-    resolve(data.address);
-  } else {
-    var res = new Error("failed......");
-    reject(res);
-  }
+      $.each(data.products, function(i, item) {
+        $("#data").append(
+          "<tr><td>" +
+            item.id +
+            "</td><td>" +
+            item.name +
+            "</td><td>" +
+            item.price +
+            "</td><td><img src='" +
+            item.image +
+            "' /></td><td>" +
+            item.product_type +
+            "</td></tr>"
+        );
+      });
+    }
+  });
 });
-
-var promise2 = function(address) {
-  var msg = address.state + " " + address.zip;
-  return Promise.resolve(msg);
-};
-
-var myFunc = function() {
-  promise1
-    .then(promise2)
-    .then(data => {
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err.message);
-    });
-};
-
-myFunc();
-//Print: ‘Marlabs address: New jersey, 08444’
